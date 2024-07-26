35951a92c115402cb700f0c60be43be4e54daaf9
cootanasy <namkhue2606@gmail.com>
2024-07-26 15:27:32 +0700

    update new UI

--- src/models/cardModel.js
+++ src/models/cardModel.js
@@ -1,76 +1,90 @@
-import Joi from 'joi'
-import { OBJECT_ID_RULE, OBJECT_ID_RULE_MESSAGE } from '~/utils/validators'
-import { ObjectId } from 'mongodb'
+import Joi from "joi";
+import { OBJECT_ID_RULE, OBJECT_ID_RULE_MESSAGE } from "~/utils/validators";
+import { ObjectId } from "mongodb";
 
-import { GET_DB } from '~/config/mongodb'
+import { GET_DB } from "~/config/mongodb";
 
 // xác định những Fields mà chúng ta không muốn cho phép cập nhật trong hàm update()
-const INVALID_UPDATE_FIELDS = ['_id', 'createdAt', 'boardId']
+const INVALID_UPDATE_FIELDS = ["_id", "createdAt", "boardId"];
 
 // Define Collection (name & schema)
-const CARD_COLLECTION_NAME = 'cards'
+const CARD_COLLECTION_NAME = "cards";
 const CARD_COLLECTION_SCHEMA = Joi.object({
-  boardId: Joi.string().required().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE),
-  columnId: Joi.string().required().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE),
+  boardId: Joi.string()
+    .required()
+    .pattern(OBJECT_ID_RULE)
+    .message(OBJECT_ID_RULE_MESSAGE),
+  columnId: Joi.string()
+    .required()
+    .pattern(OBJECT_ID_RULE)
+    .message(OBJECT_ID_RULE_MESSAGE),
 
   title: Joi.string().required().min(3).max(50).trim().strict(),
   description: Joi.string().optional(),
 
-  createdAt: Joi.date().timestamp('javascript').default(Date.now),
-  updatedAt: Joi.date().timestamp('javascript').default(null),
-  _destroy: Joi.boolean().default(false)
-})
+  createdAt: Joi.date().timestamp("javascript").default(Date.now),
+  updatedAt: Joi.date().timestamp("javascript").default(null),
+  _destroy: Joi.boolean().default(false),
+});
 
 const validateBeforeCreating = async (data) => {
-  return await CARD_COLLECTION_SCHEMA.validateAsync(data, { abortEarly: false })
-}
+  return await CARD_COLLECTION_SCHEMA.validateAsync(data, {
+    abortEarly: false,
+  });
+};
 
 const createNew = async (data) => {
   try {
-    const validData = await validateBeforeCreating(data)
-    
+    const validData = await validateBeforeCreating(data);
+
     // biến đổi dữ liệu từ string sang objectId để lưu vào DB
     const newCardToAdd = {
       ...validData,
       boardId: new ObjectId(validData.boardId),
-      columnId: new ObjectId(validData.columnId)
-    }
-    
-    const createdCard = await GET_DB().collection(CARD_COLLECTION_NAME).insertOne(newCardToAdd)
+      columnId: new ObjectId(validData.columnId),
+    };
 
-    return createdCard
+    const createdCard = await GET_DB()
+      .collection(CARD_COLLECTION_NAME)
+      .insertOne(newCardToAdd);
+
+    return createdCard;
   } catch (error) {
-    throw new Error(error)
+    throw new Error(error);
   }
-}
+};
 
 const findOneById = async (cardId) => {
   try {
-    const result = await GET_DB().collection(CARD_COLLECTION_NAME).findOne({
-      _id: new ObjectId(cardId)
-    })
+    const result = await GET_DB()
+      .collection(CARD_COLLECTION_NAME)
+      .findOne({
+        _id: new ObjectId(cardId),
+      });
 
-    return result
+    return result;
   } catch (error) {
-    throw new Error(error)
+    throw new Error(error);
   }
-}
+};
 
 const update = async (cardId, updateData) => {
   try {
     // lọc ra những field khong được phép cập nhật
-    Object.keys(updateData).forEach(fieldName => {
+    Object.keys(updateData).forEach((fieldName) => {
       if (INVALID_UPDATE_FIELDS.includes(fieldName)) {
-        delete updateData[fieldName]
+        delete updateData[fieldName];
       }
-    })
+    });
+
+    // console.log("updateData.columnId:", updateData.columnId);
 
     // đối với những dữ liệu liên quan đến ObjectId, thì biến đổi ở đây
     if (updateData.columnId) {
-      updateData.columnId = new ObjectId(updateData.columnId)
+      updateData.columnId = new ObjectId(updateData.columnId);
     }
-    
-    // console.log('updateData:', updateData)
+
+    // console.log("updateData:", updateData);
 
     // đọc findOneAndUpdate
     const result = await GET_DB()
@@ -78,65 +92,76 @@ const update = async (cardId, updateData) => {
       .findOneAndUpdate(
         {
           // 67 - có cần destroy khong?
-          _id: new ObjectId(cardId)
+          _id: new ObjectId(cardId),
         },
         {
-          $set: updateData
+          $set: updateData,
         },
         {
-          returnDocument: 'after' // bắt buộc cần thì mới trả về DB đã được cập nhật
+          returnDocument: "after", // bắt buộc cần thì mới trả về DB đã được cập nhật
         }
-      )
+      );
 
-    return result
+    return result;
   } catch (error) {
-    throw new Error(error)
+    throw new Error(error);
   }
-}
+};
 
 const deleteManyByColumnId = async (columnId) => {
   try {
-    const result = await GET_DB().collection(CARD_COLLECTION_NAME).deleteMany({
-      columnId: new ObjectId(columnId)
-    })
+    const result = await GET_DB()
+      .collection(CARD_COLLECTION_NAME)
+      .deleteMany({
+        columnId: new ObjectId(columnId),
+      });
 
     // console.log('deleteManyByColumnIdResult:', result)
 
-    return result
+    return result;
   } catch (error) {
-    throw new Error(error)
+    throw new Error(error);
   }
-}
+};
 
 const deleteOneById = async (cardId) => {
   try {
-    const result = await GET_DB().collection(CARD_COLLECTION_NAME).deleteOne({
-      _id: new ObjectId(cardId)
-    })
+    const result = await GET_DB()
+      .collection(CARD_COLLECTION_NAME)
+      .deleteOne({
+        _id: new ObjectId(cardId),
+      });
 
     // console.log('deleteOneByIdResult:', result)
 
-    return result
+    return result;
   } catch (error) {
-    throw new Error(error)
+    throw new Error(error);
   }
-}
+};
 
 const editCard = async (cardId, updateData) => {
   try {
     // lọc ra những field khong được phép cập nhật
-    Object.keys(updateData).forEach(fieldName => {
+    Object.keys(updateData).forEach((fieldName) => {
       if (INVALID_UPDATE_FIELDS.includes(fieldName)) {
-        delete updateData[fieldName]
+        delete updateData[fieldName];
       }
-    })
+    });
+
+    // console.log("updateData.columnId:", updateData.columnId);
 
     // đối với những dữ liệu liên quan đến ObjectId, thì biến đổi ở đây
+    if (updateData.columnId) {
+      updateData.columnId = new ObjectId(updateData.columnId);
+    }
     // if (updateData.cardOrderIds) {
-    //   updateData.cardOrderIds = updateData.cardOrderIds.map(_id => (new ObjectId(_id)))
+    //   updateData.cardOrderIds = updateData.cardOrderIds.map(
+    //     (_id) => new ObjectId(_id)
+    //   );
     // }
-    
-    // console.log('updateData:', updateData)
+
+    // console.log("updateData:", updateData);
 
     // đọc findOneAndUpdate
     const result = await GET_DB()
@@ -144,21 +169,21 @@ const editCard = async (cardId, updateData) => {
       .findOneAndUpdate(
         {
           // 67 - có cần destroy khong?
-          _id: new ObjectId(cardId)
+          _id: new ObjectId(cardId),
         },
         {
-          $set: updateData
+          $set: updateData,
         },
         {
-          returnDocument: 'after' // bắt buộc cần thì mới trả về DB đã được cập nhật
+          returnDocument: "after", // bắt buộc cần thì mới trả về DB đã được cập nhật
         }
-      )
+      );
 
-    return result
+    return result;
   } catch (error) {
-    throw new Error(error)
+    throw new Error(error);
   }
-}
+};
 
 export const cardModel = {
   CARD_COLLECTION_NAME,
@@ -168,5 +193,5 @@ export const cardModel = {
   update,
   deleteManyByColumnId,
   deleteOneById,
-  editCard
-}
+  editCard,
+};
